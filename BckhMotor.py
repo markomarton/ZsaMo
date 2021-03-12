8a463df1a6bb64fa753471908e502055c754c09f
ATHOS <szakal.alex@gmail.com>
2021-03-12 09:45:11 +0100

    PLC variable names changed due to new SW in PLC

--- BckhMotor.py
+++ BckhMotor.py
@@ -23,64 +23,64 @@ class BckhMotor:
         #plc.write_by_name("GVL.axes[{}].control.bEnable".format(self.MotNum), True, pyads.PLCTYPE_BOOL)
         
         if Speed>0:
-            plc.write_by_name("GVL.axes[{}].config.fVelocity".format(self.MotNum), Speed, pyads.PLCTYPE_LREAL)
+            plc.write_by_name("GVL.astAxes[{}].stControl.fVelocity".format(self.MotNum), Speed, pyads.PLCTYPE_LREAL)
         else:
-            self.Speed = plc.read_by_name("GVL.axes[{}].config.fVelocity".format(self.MotNum), pyads.PLCTYPE_LREAL)
+            self.Speed = plc.read_by_name("GVL.astAxes[{}].stControl.fVelocity".format(self.MotNum), pyads.PLCTYPE_LREAL)
 
         if Acc>0:
-            plc.write_by_name("GVL.axes[{}].config.fAcceleration".format(self.MotNum), Acc, pyads.PLCTYPE_LREAL)
+            plc.write_by_name("GVL.astAxes[{}].stControl.fAcceleration".format(self.MotNum), Acc, pyads.PLCTYPE_LREAL)
         else:
-            self.Acc = plc.read_by_name("GVL.axes[{}].config.fAcceleration".format(self.MotNum), pyads.PLCTYPE_LREAL)
+            self.Acc = plc.read_by_name("GVL.astAxes[{}].stControl.fAcceleration".format(self.MotNum), pyads.PLCTYPE_LREAL)
 
         if Dec>0:
-            plc.write_by_name("GVL.axes[{}].config.fDeceleration".format(self.MotNum), Dec, pyads.PLCTYPE_LREAL)
+            plc.write_by_name("GVL.astAxes[{}].stControl.fDeceleration".format(self.MotNum), Dec, pyads.PLCTYPE_LREAL)
         else:
-            self.Dec = plc.read_by_name("GVL.axes[{}].config.fDeceleration".format(self.MotNum), pyads.PLCTYPE_LREAL)
+            self.Dec = plc.read_by_name("GVL.astAxes[{}].stControl.fDeceleration".format(self.MotNum), pyads.PLCTYPE_LREAL)
 
 
-        plc.write_by_name("GVL.axes[{}].control.eCommand".format(self.MotNum), 0, pyads.PLCTYPE_INT)
+        plc.write_by_name("GVL.astAxes[{}].stControl.eCommand".format(self.MotNum), 0, pyads.PLCTYPE_INT)
         pass
 
 
     def getPosition(self):
         reply = 'Positon: ' + self.MotName + ' ,'
-        reply += str(self.plc.read_by_name("GVL.axes[{}].status.bBusy".format(self.MotNum), pyads.PLCTYPE_BOOL))
+        reply += str(self.plc.read_by_name("GVL.astAxes[{}].stStatus.bBusy".format(self.MotNum), pyads.PLCTYPE_BOOL))
         reply += ','
-        reply += str(self.plc.read_by_name("GVL.axes[{}].status.nErrorID".format(self.MotNum), pyads.PLCTYPE_UDINT))
+        reply += str(self.plc.read_by_name("GVL.astAxes[{}].stStatus.nErrorID".format(self.MotNum), pyads.PLCTYPE_UDINT))
         reply += ','
-        reply += "{:.2f}".format(float(self.plc.read_by_name("GVL.axes[{}].status.fActPosition".format(self.MotNum), pyads.PLCTYPE_LREAL)))
+        reply += "{:.2f}".format(float(self.plc.read_by_name("GVL.astAxes[{}].stStatus.fActPosition".format(self.MotNum), pyads.PLCTYPE_LREAL)))
         reply += ','
-        reply += str(self.plc.read_by_name("GVL.axes[{}].config.fPosition".format(self.MotNum), pyads.PLCTYPE_LREAL))
+        reply += str(self.plc.read_by_name("GVL.astAxes[{}].stControl.fPosition".format(self.MotNum), pyads.PLCTYPE_LREAL))
         reply += ';'
         return reply
         pass
 
     def move(self,targetPos):
-        self.plc.write_by_name("GVL.axes[{}].control.bEnable".format(self.MotNum), True, pyads.PLCTYPE_BOOL)
-        self.plc.write_by_name("GVL.axes[{}].control.bStop".format(self.MotNum), False, pyads.PLCTYPE_BOOL)
-        self.plc.write_by_name("GVL.axes[{}].control.eCommand".format(self.MotNum), 0, pyads.PLCTYPE_INT)
-        self.plc.write_by_name("GVL.axes[{}].config.fPosition".format(self.MotNum), float(targetPos), pyads.PLCTYPE_LREAL)
-        self.plc.write_by_name("GVL.axes[{}].control.bExecute".format(self.MotNum), True, pyads.PLCTYPE_BOOL)
+        self.plc.write_by_name("GVL.astAxes[{}].stControl.bEnable".format(self.MotNum), True, pyads.PLCTYPE_BOOL)
+        self.plc.write_by_name("GVL.astAxes[{}].stControl.bStop".format(self.MotNum), False, pyads.PLCTYPE_BOOL)
+        self.plc.write_by_name("GVL.astAxes[{}].stControl.eCommand".format(self.MotNum), 0, pyads.PLCTYPE_INT)
+        self.plc.write_by_name("GVL.astAxes[{}].stControl.fPosition".format(self.MotNum), float(targetPos), pyads.PLCTYPE_LREAL)
+        self.plc.write_by_name("GVL.astAxes[{}].stControl.bExecute".format(self.MotNum), True, pyads.PLCTYPE_BOOL)
         return True #with v1
         pass
 
     def moving(self):
-        return self.plc.read_by_name("GVL.axes[{}].status.bBusy".format(self.MotNum), pyads.PLCTYPE_BOOL)
+        return self.plc.read_by_name("GVL.astAxes[{}].stStatus.bBusy".format(self.MotNum), pyads.PLCTYPE_BOOL)
     
     def stop(self):
-        self.plc.write_by_name("GVL.axes[{}].control.bStop".format(self.MotNum), True, pyads.PLCTYPE_BOOL)
+        self.plc.write_by_name("GVL.astAxes[{}].stControl.bStop".format(self.MotNum), True, pyads.PLCTYPE_BOOL)
         
-    def status(self):
+    #def status(self):
         #not correct line
-        self.plc.write_by_name("GVL.axes[{}].status.bStatatus".format(self.MotNum), True, pyads.PLCTYPE_BOOL)
+    #    self.plc.write_by_name("GVL.axes[{}].status.bStatus".format(self.MotNum), True, pyads.PLCTYPE_BOOL)
         
     def reset(self):
-        self.plc.write_by_name("GVL.axes[{}].control.bReset".format(self.MotNum), True, pyads.PLCTYPE_BOOL)
+        self.plc.write_by_name("GVL.astAxes[{}].stControl.bReset".format(self.MotNum), True, pyads.PLCTYPE_BOOL)
     
     def homeAxis(self):
-        self.plc.write_by_name("GVL.axes[{}].control.eCommand".format(self.MotNum), 10, pyads.PLCTYPE_INT) # 10 -> Motionfunctions::Home
-        self.plc.write_by_name("GVL.axes[{}].config.nHomeSeq".format(self.MotNum),  1,  pyads.PLCTYPE_UINT) # 1 -> HomeToLowLimit
-        self.plc.write_by_name("GVL.axes[{}].control.bExecute".format(self.MotNum), True, pyads.PLCTYPE_BOOL)
+        self.plc.write_by_name("GVL.astAxes[{}].stControl.eCommand".format(self.MotNum), 10, pyads.PLCTYPE_INT) # 10 -> Motionfunctions::Home
+        self.plc.write_by_name("GVL.astAxes[{}].stConfig.nHomeSeq".format(self.MotNum),  1,  pyads.PLCTYPE_UINT) # 1 -> HomeToLowLimit
+        self.plc.write_by_name("GVL.astAxes[{}].stControl.bExecute".format(self.MotNum), True, pyads.PLCTYPE_BOOL)
 
 
 
